2bd2cf7d3ad6d95ceb81de5c0af8adee9164ed70
1-8192 <allegranzia@gmail.com>
2019-01-30 16:10:04 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(seconds_to_go)
+  while seconds_to_go > 0
+    puts "#{seconds_to_go} SECOND(S)!"
+    seconds_to_go = seconds_to_go - 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds_to_go)
+  while seconds_to_go > 0
+    puts "#{seconds_to_go} SECOND(S)!"
+    seconds_to_go = seconds_to_go - 1
+    sleep 1
+  end
+  return "HAPPY NEW YEAR!"
 end
